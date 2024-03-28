ad9607437b7b1257a44a49759366291952f88148
Junaid Tariq <ijunaidev@gmail.com>
2024-03-29 01:16:35 +0500

    Initial Commit

---
+++ src/main/java/com/genesis/university/UniversityAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.genesis.university;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityAssessmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniversityAssessmentApplication.class, args);
+    }
+
+}
